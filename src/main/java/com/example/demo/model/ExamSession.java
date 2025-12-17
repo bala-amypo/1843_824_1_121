947c68b07540c3f2c49ef3ddbea5947c8415936d
bala-amypo <bala@amypo.in>
2025-12-17 09:45:37 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamSession.java
+++ src/main/java/com/example/demo/model/ExamSession.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
 
 @Entity
 public class ExamSession{
@@ -13,5 +14,45 @@ public class ExamSession{
     private String courseCode;
     private LocalDate examDate;
     private String examTime;
+
+    public Long getId() {
+        return id;
+    }
+
+   
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+   
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+ 
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
     
+    public LocalDate getExamDate() {
+        return examDate;
+    }
+
+    
+    public void setExamDate(LocalDate examDate) {
+        this.examDate = examDate;
+    }
+
+   
+    public String getExamTime() {
+        return examTime;
+    }
+
+ 
+    public void setExamTime(String examTime) {
+        this.examTime = examTime;
+    }
+
+    public ExamSession(Long id,String courseCode,LocalDate examDate)
 }
