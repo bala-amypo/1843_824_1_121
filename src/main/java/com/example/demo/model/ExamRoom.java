00604c84d8c85bbee4595fffd0839ba7da77f8d3
bala-amypo <bala@amypo.in>
2025-12-17 09:35:37 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamRoom.java
+++ src/main/java/com/example/demo/model/ExamRoom.java
@@ -12,5 +12,60 @@ public class ExamRoom{
     private Long id;
 
     @Column(unique=true)
+    private String roomNumber;
+
+    private Integer capacity;
+    private Integer rows;
+    private Integer columns;
     
+     public Long getId() {
+        return id;
+    }
+
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+   
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+   
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+   
+    public Integer getCapacity() {
+        return Capacity;
+    }
+
+
+    public void setCapacity(Integer Capacity) {
+        this.Capacity = Capacity;
+    }
+
+   
+    public Integer getRows() {
+        return rows;
+    }
+
+   
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+   
+    public Integer getColumns() {
+        return columns;
+    }
+
+  
+    public void setColumns(Integer columns) {
+        this.columns = columns;
+    }
+
+    public ExamRoom(Long id,String roomNumber,Integer Capacity)
 }
