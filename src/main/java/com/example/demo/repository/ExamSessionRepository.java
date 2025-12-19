2724da66d6503aa309ad6610168b4a2defd2ee11
bala-amypo <bala@amypo.in>
2025-12-19 08:08:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamSessionRepository.java
+++ src/main/java/com/example/demo/repository/ExamSessionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.ExamSession;
 
 public interface ExamSessionRepository extends JpaRepository<ExamSession, Long>{
-
+   
 }
