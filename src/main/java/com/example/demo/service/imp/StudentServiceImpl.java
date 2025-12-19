6c04098cfcd4312af5a60cef6aec47e6ae7ed76c
bala-amypo <bala@amypo.in>
2025-12-19 04:54:46 +0000

    User commit

--- src/main/java/com/example/demo/service/imp/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/imp/StudentServiceImpl.java
@@ -2,9 +2,9 @@ package com.example.demo.service.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.ApiException;
 import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
@@ -12,11 +12,24 @@ import com.example.demo.service.StudentService;
 @Service
 public class StudentServiceImpl implements StudentService {
 
-    @Autowired
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
+
+    // REQUIRED constructor order
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     @Override
     public Student addStudent(Student student) {
+
+        if (studentRepository.findByRollNumber(student.getRollNumber()).isPresent()) {
+            throw new ApiException("roll number exists");
+        }
+
+        if (student.getYear() < 1 || student.getYear() > 5) {
+            throw new ApiException("invalid year");
+        }
+
         return studentRepository.save(student);
     }
 
