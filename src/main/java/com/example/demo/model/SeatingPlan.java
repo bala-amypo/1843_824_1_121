be5e86af68f9ff0d0325af4c4f210938f6772451
bala-amypo <bala@amypo.in>
2025-12-19 09:32:03 +0000

    User commit

--- src/main/java/com/example/demo/model/SeatingPlan.java
+++ src/main/java/com/example/demo/model/SeatingPlan.java
@@ -1,7 +1,15 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+
 
 @Entity
 public class SeatingPlan {
