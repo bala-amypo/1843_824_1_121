3406593efde05ed32d8f3a8cbfdfacb0b850f509
bala-amypo <bala@amypo.in>
2025-12-19 05:59:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/ExamSessionController.java
+++ src/main/java/com/example/demo/controller/ExamSessionController.java
@@ -1,28 +1,28 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import org.springframework.web.bind.annotation.*;
-// import com.example.demo.model.ExamSession;
-// import com.example.demo.service.ExamSessionService;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.ExamSession;
+import com.example.demo.service.ExamSessionService;
 
-// @RestController
-// @RequestMapping("/sessions")
-// public class ExamSessionController {
+@RestController
+@RequestMapping("/sessions")
+public class ExamSessionController {
 
-//     private final ExamSessionService examSessionService;
+    private final ExamSessionService examSessionService;
 
-//     public ExamSessionController(ExamSessionService examSessionService) {
-//         this.examSessionService = examSessionService;
-//     }
+    public ExamSessionController(ExamSessionService examSessionService) {
+        this.examSessionService = examSessionService;
+    }
 
     
-//     @PostMapping
-//     public ExamSession createSession(@RequestBody ExamSession session) {
-//         return examSessionService.createSession(session);
-//     }
+    @PostMapping
+    public ExamSession createSession(@RequestBody ExamSession session) {
+        return examSessionService.createSession(session);
+    }
 
 
-//     @GetMapping("/{sessionId}")
-//     public ExamSession getSession(@PathVariable Long sessionId) {
-//         return examSessionService.getSession(sessionId);
-//     }
-// }
+    @GetMapping("/{sessionId}")
+    public ExamSession getSession(@PathVariable Long sessionId) {
+        return examSessionService.getSession(sessionId);
+    }
+}
