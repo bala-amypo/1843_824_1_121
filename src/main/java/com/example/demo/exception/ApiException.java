bf5a7a4d00ec3d215608873540ba33c64bf2242e
bala-amypo <bala@amypo.in>
2025-12-19 05:00:46 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -1,3 +1,9 @@
-public class ApiException{
-    
+package com.example.demo.exception;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String message) {
+        super(message);
+    }
 }
+
