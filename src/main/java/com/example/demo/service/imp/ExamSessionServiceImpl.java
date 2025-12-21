eb029c4b3d49c82a9bc533ac22d285be395b30a8
bala-amypo <bala@amypo.in>
2025-12-21 14:39:54 +0000

    User commit

--- src/main/java/com/example/demo/service/imp/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/imp/ExamSessionServiceImpl.java
@@ -2,17 +2,25 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.repository.ExamSessionRepository;
 import com.example.demo.model.ExamSession;
+import com.example.demo.repository.ExamSessionRepository;
+import java.util.Optional;
 
 @Service
-public class ExamSessionServiceImpl {
+public class ExamSessionServiceImpl implements ExamSessionService {
 
     @Autowired
     private ExamSessionRepository examSessionRepository;
 
+    @Override
     public ExamSession createSession(ExamSession examSession) {
         // JPA handles students because of CascadeType.PERSIST/MERGE
         return examSessionRepository.save(examSession);
+    }
+
+    @Override
+    public ExamSession getSession(Long sessionId) {
+        Optional<ExamSession> session = examSessionRepository.findById(sessionId);
+        return session.orElse(null); // or throw exception if you want
     }
 }
