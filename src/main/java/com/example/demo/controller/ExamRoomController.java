e84853826b832a8263fc98626b21407c91ad282a
bala-amypo <bala@amypo.in>
2025-12-26 08:40:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/ExamRoomController.java
+++ src/main/java/com/example/demo/controller/ExamRoomController.java
@@ -1,35 +1,4 @@
-// package com.example.demo.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-
-// import com.example.demo.model.ExamRoom;
-// import com.example.demo.service.ExamRoomService;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/exam-rooms")
-// public class ExamRoomController {
-
-//     private final ExamRoomService examRoomService;
-
-//     @Autowired
-//     public ExamRoomController(ExamRoomService examRoomService) {
-//         this.examRoomService = examRoomService;
-//     }
-
-    
-//     @PostMapping
-//     public ExamRoom addRoom(@RequestBody ExamRoom examRoom) {
-//         return examRoomService.addRoom(examRoom);
-//     }
-
-//     @GetMapping
-//     public List<ExamRoom> getAllRooms() {
-//         return examRoomService.getAllRooms();
-//     }
-// }
 package com.example.demo.controller;
 
 import java.util.List;
