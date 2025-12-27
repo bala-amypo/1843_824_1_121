a7609cf53ff664d65adeace4f8bad2ee701d57cf
bala-amypo <bala@amypo.in>
2025-12-27 04:03:08 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamRoom.java
+++ src/main/java/com/example/demo/model/ExamRoom.java
@@ -72,7 +72,7 @@ public class ExamRoom {
     private Long id;
 
     @Column(unique = true)
-    private String RoomNumber;
+    private String roomNumber;
 
     private Integer rows;
     private Integer columns;
