8a1e63939491a69c322d1f4df00866bbec4b645c
bala-amypo <bala@amypo.in>
2025-12-27 04:15:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,38 +1,6 @@
 
-// package com.example.demo.service.impl;
-
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Service;
-
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.UserService;
-
-// @Service
-// public class UserServiceImpl implements UserService {
-
-//     private final UserRepository userRepository;
-//     private final PasswordEncoder passwordEncoder;
-
-//     // ✅ TEST EXPECTS THIS CONSTRUCTOR
-//     public UserServiceImpl(UserRepository userRepository,
-//                            PasswordEncoder passwordEncoder) {
-//         this.userRepository = userRepository;
-//         this.passwordEncoder = passwordEncoder;
-//     }
-
-//     @Override
-//     public User register(User user) {
-//         user.setPassword(passwordEncoder.encode(user.getPassword()));
-//         return userRepository.save(user);
-//     }
-
-//     @Override
-//     public User findByEmail(String email) {
-//         return userRepository.findByEmail(email).orElse(null);
-//     }
-// }
 package com.example.demo.service.impl;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.service.UserService;
 import com.example.demo.repository.UserRepository;
@@ -40,7 +8,7 @@ import com.example.demo.model.User;
 import com.example.demo.exception.ApiException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+@Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
