0f46d7c57ee4d56bd12269f07e7e1d6a3c8aea95
bala-amypo <bala@amypo.in>
2025-12-29 04:53:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,34 +1,4 @@
-// package com.example.demo.controller;
 
-// import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.demo.model.Student;
-// import com.example.demo.service.StudentService;
-
-// @RestController
-// @RequestMapping("/students")
-// public class StudentController {
-
-//     @Autowired
-//     private StudentService studentService;
-
-//     @PostMapping
-//     public Student saveStudent(@RequestBody Student student) {
-//         return studentService.saveStudent(student);
-//     }
-
-//     @GetMapping
-//     public List<Student> getAllStudents() {
-//         return studentService.getAllStudents();
-//     }
-// }
 package com.example.demo.controller;
 
 import com.example.demo.service.StudentService;
--- src/main/java/com/example/demo/repository/ExamRoomRepository.java
+++ src/main/java/com/example/demo/repository/ExamRoomRepository.java
@@ -1,12 +1,4 @@
-// package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.model.ExamRoom;
-// import java.util.Optional;
-
-// public interface ExamRoomRepository extends JpaRepository<ExamRoom, Long> {
-//     Optional<ExamRoom> findByRoomNumber(String roomNumber);
-// }
 package com.example.demo.repository;
 
 import com.example.demo.model.ExamRoom;
